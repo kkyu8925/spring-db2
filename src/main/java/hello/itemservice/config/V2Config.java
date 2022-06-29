043d3c62ba6e00b22f9b1e10559eaeb7382047b0
kkyu8925 <kkyu8925@gmail.com>
2022-06-29 23:20:22 +0900

    데이터 접근 기술 - Querydsl

--- src/main/java/hello/itemservice/config/V2Config.java
+++ src/main/java/hello/itemservice/config/V2Config.java
@@ -1,13 +1,10 @@
 package hello.itemservice.config;
 
 import hello.itemservice.repository.ItemRepository;
-import hello.itemservice.repository.jpa.JpaItemRepositoryV2;
 import hello.itemservice.repository.jpa.JpaItemRepositoryV3;
-import hello.itemservice.repository.jpa.SpringDataJpaItemRepository;
 import hello.itemservice.repository.v2.ItemQueryRepositoryV2;
 import hello.itemservice.repository.v2.ItemRepositoryV2;
 import hello.itemservice.service.ItemService;
-import hello.itemservice.service.ItemServiceV1;
 import hello.itemservice.service.ItemServiceV2;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
